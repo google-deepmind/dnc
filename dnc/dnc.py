ef8c9bbe641b1592a3680bd9fa32b554985f9456
Rik Bauwens <rikbauwens1@gmail.com>
2018-12-06 13:28:10 +0100

    Output read/write weightings

--- dnc/dnc.py
+++ dnc/dnc.py
@@ -44,7 +44,8 @@ class DNC(snt.RNNCore):
                controller_config,
                output_size,
                clip_value=None,
-               name='dnc'):
+               name='dnc',
+               return_weights=False):
     """Initializes the DNC core.
 
     Args:
@@ -61,12 +62,19 @@ class DNC(snt.RNNCore):
     """
     super(DNC, self).__init__(name=name)
 
+    self._memory_size = access_config.get('memory_size')
+    self._nb_extra_output = 2 * self._memory_size
+
     with self._enter_variable_scope():
       self._controller = snt.LSTM(**controller_config)
       self._access = access.MemoryAccess(**access_config)
 
     self._access_output_size = np.prod(self._access.output_size.as_list())
+    if return_weights:
+       output_size += self._nb_extra_output
+
     self._output_size = output_size
+
     self._clip_value = clip_value or 0
 
     self._output_size = tf.TensorShape([output_size])
@@ -74,6 +82,8 @@ class DNC(snt.RNNCore):
         access_output=self._access_output_size,
         access_state=self._access.state_size,
         controller_state=self._controller.state_size)
+
+    self._return_weights = return_weights
 
   def _clip_if_enabled(self, x):
     if self._clip_value > 0:
@@ -116,10 +126,20 @@ class DNC(snt.RNNCore):
                                                prev_access_state)
 
     output = tf.concat([controller_output, batch_flatten(access_output)], 1)
+    output_size = self._output_size.as_list()[0]
+    if self._return_weights:
+      output_size -= self._nb_extra_output
+
     output = snt.Linear(
-        output_size=self._output_size.as_list()[0],
+        output_size=output_size,
         name='output_linear')(output)
     output = self._clip_if_enabled(output)
+
+    write_weights = access_state.write_weights
+    read_weights = access_state.read_weights
+
+    if self._return_weights:
+      output = tf.concat([output, read_weights[:, 0, :], write_weights[:, 0, :]], 1)
 
     return output, DNCState(
         access_output=access_output,
--- train.py
+++ train.py
@@ -23,6 +23,7 @@ import sonnet as snt
 
 from dnc import dnc
 from dnc import repeat_copy
+from tasks import repeat_sequence
 
 FLAGS = tf.flags.FLAGS
 
@@ -31,7 +32,7 @@ tf.flags.DEFINE_integer("hidden_size", 64, "Size of LSTM hidden layer.")
 tf.flags.DEFINE_integer("memory_size", 16, "The number of memory slots.")
 tf.flags.DEFINE_integer("word_size", 16, "The width of each memory slot.")
 tf.flags.DEFINE_integer("num_write_heads", 1, "Number of memory write heads.")
-tf.flags.DEFINE_integer("num_read_heads", 4, "Number of memory read heads.")
+tf.flags.DEFINE_integer("num_read_heads", 1, "Number of memory read heads.")
 tf.flags.DEFINE_integer("clip_value", 20,
                         "Maximum absolute value of controller and dnc outputs.")
 
@@ -60,13 +61,13 @@ tf.flags.DEFINE_integer("num_training_iterations", 100000,
                         "Number of iterations to train for.")
 tf.flags.DEFINE_integer("report_interval", 100,
                         "Iterations between reports (samples, valid loss).")
-tf.flags.DEFINE_string("checkpoint_dir", "/tmp/tf/dnc",
+tf.flags.DEFINE_string("checkpoint_dir", "./checkpoint",
                        "Checkpointing directory.")
-tf.flags.DEFINE_integer("checkpoint_interval", -1,
+tf.flags.DEFINE_integer("checkpoint_interval", 1000,
                         "Checkpointing step interval.")
+tf.flags.DEFINE_string("summary_dir", "./summaries", "Summary directoyu")
 
-
-def run_model(input_sequence, output_size):
+def run_model(input_sequence, output_size, return_weights, time_major=False):
   """Runs model on input sequence."""
 
   access_config = {
@@ -80,13 +81,14 @@ def run_model(input_sequence, output_size):
   }
   clip_value = FLAGS.clip_value
 
-  dnc_core = dnc.DNC(access_config, controller_config, output_size, clip_value)
+  dnc_core = dnc.DNC(access_config, controller_config, output_size, clip_value, return_weights=return_weights)
   initial_state = dnc_core.initial_state(FLAGS.batch_size)
   output_sequence, _ = tf.nn.dynamic_rnn(
       cell=dnc_core,
       inputs=input_sequence,
-      time_major=True,
-      initial_state=initial_state)
+      time_major=time_major,
+      initial_state=initial_state
+  )
 
   return output_sequence
 
@@ -99,13 +101,33 @@ def train(num_training_iterations, report_interval):
                                    FLAGS.min_repeats, FLAGS.max_repeats)
   dataset_tensors = dataset()
 
-  output_logits = run_model(dataset_tensors.observations, dataset.target_size)
-  # Used for visualization.
-  output = tf.round(
-      tf.expand_dims(dataset_tensors.mask, -1) * tf.sigmoid(output_logits))
+  output_concat = run_model(dataset_tensors.observations, dataset.target_size, True, time_major=dataset.time_major())
 
-  train_loss = dataset.cost(output_logits, dataset_tensors.target,
-                            dataset_tensors.mask)
+  output_logits = output_concat[:, :, 0:dataset.target_size]
+  output_read_weightings = output_concat[:, :, dataset.target_size:(dataset.target_size+FLAGS.memory_size)]
+  output_write_weightings = output_concat[:, :, (dataset.target_size+FLAGS.memory_size):(dataset.target_size+2*FLAGS.memory_size)]
+
+  # Used for visualization.
+  # output = tf.round(
+  #     tf.expand_dims(dataset_tensors.mask, -1) * tf.sigmoid(output_logits))
+
+  output = tf.round(tf.sigmoid(output_logits))
+
+  print('Target: ')
+  print(dataset_tensors.target)
+  print('\nLogits: ')
+  print(output_logits)
+
+  train_loss = dataset.cost(output_logits, dataset_tensors.target)
+
+  tf.summary.image('Input', tf.expand_dims(dataset_tensors.observations, 3))
+  tf.summary.image('Target', tf.expand_dims(dataset_tensors.target, 3))
+  tf.summary.image('Output', tf.expand_dims(output_logits, 3))
+  tf.summary.image('Read_weightings', tf.expand_dims(output_read_weightings, 3))
+  tf.summary.image('Write_weightings', tf.expand_dims(output_write_weightings, 3))
+  tf.summary.histogram('Loss', train_loss)
+
+  merged = tf.summary.merge_all()
 
   # Set up optimizer with global norm clipping.
   trainable_variables = tf.trainable_variables()
@@ -141,22 +163,31 @@ def train(num_training_iterations, report_interval):
   with tf.train.SingularMonitoredSession(
       hooks=hooks, checkpoint_dir=FLAGS.checkpoint_dir) as sess:
 
+    train_writer = tf.summary.FileWriter(FLAGS.summary_dir, sess.graph)
+
     start_iteration = sess.run(global_step)
     total_loss = 0
 
     for train_iteration in range(start_iteration, num_training_iterations):
-      _, loss = sess.run([train_step, train_loss])
+      _, loss, summary = sess.run([train_step, train_loss, merged])
       total_loss += loss
 
       if (train_iteration + 1) % report_interval == 0:
         dataset_tensors_np, output_np = sess.run([dataset_tensors, output])
         dataset_string = dataset.to_human_readable(dataset_tensors_np,
                                                    output_np)
+
         tf.logging.info("%d: Avg training loss %f.\n%s",
                         train_iteration, total_loss / report_interval,
                         dataset_string)
         total_loss = 0
 
+        train_writer.add_summary(summary, train_iteration)
+
+# def run_saved():
+
+def to_batch_major(tensor):
+  return tf.transpose(tensor, [1, 0, 2])
 
 def main(unused_argv):
   tf.logging.set_verbosity(3)  # Print INFO log messages.
